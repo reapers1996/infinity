03f42c427a760022f91c7517116a9900211cea7e
reapers1996 <damien.forest.via@gmail.com>
2022-06-07 14:28:48 +0200

    j

--- template/base.php
+++ template/base.php
@@ -1,14 +1,31 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
+
 <head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Page Title</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$title?></title>
 </head>
+
 <body>
-    
+    <header>
+        <div id="titre">
+            <img src="/infinity/assets/images/logoblanc.png">
+            <h1>Infinity</h1>
+        </div>
+    </header>
+    <nav>
+        1234
+    </nav>
+    <main>
+        Bienvenue sur Infinity ! Actualités gaming, classements de jeux
+        hebdomadaires, offres à ne pas manquer...quelle que soit votre
+        plateforme préférée, nous sommes là pour vous guider !
+    </main>
+    <footer>
+    <?=$content?>
+    </footer>
 </body>
+
 </html>
--- template/homepage.php
+++ template/homepage.php
@@ -1,37 +1,14 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <header>
-        <div id="titre">
-            <img src="/infinity/assets/images/logoblanc.png">
-            <h1>Infinity</h1>
-        </div>
-    </header>
-    <nav>
-        1234
-    </nav>
-    <main>
-        Bienvenue sur Infinity ! Actualités gaming, classements de jeux
-        hebdomadaires, offres à ne pas manquer...quelle que soit votre
-        plateforme préférée, nous sommes là pour vous guider !
-    </main>
-    <footer>
-        Pour ne rien manquer, abonnez-vous à notre Newsletter:
-        <form method="POST">
-            <div><label for="email"></label> <input id="email" placeholder="Votre adresse e-mail" name="subscriber_email" type="email" />
-                <button type="submit">S'abonner</button>
-            </div>
-        </form>
-        Retrouvez-nous sur les réseaux !
-    </footer>
-</body>
-
-</html>
+<?php
+$title = "Infinity_accueil";
+ob_start();
+?>
+Pour ne rien manquer, abonnez-vous à notre Newsletter:
+<form method="POST">
+    <div><label for="email"></label> <input id="email" placeholder="Votre adresse e-mail" name="subscriber_email" type="email" />
+        <button type="submit">S'abonner</button>
+    </div>
+</form>
+Retrouvez-nous sur les réseaux !
+<?php
+$content=ob_get_clean();
+require_once 'base.php'
