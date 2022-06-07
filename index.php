3999c189d03329434944677f15fe82c008cd008a
SarahBddn <sarah.bedreddine.via@gmail.com>
2022-06-07 11:33:12 +0200

    hn
    hs

--- index.php
+++ index.php
@@ -3,5 +3,9 @@ require_once($_SERVER['DOCUMENT_ROOT']."/infinity/controllers/controller.php");
 require($_SERVER['DOCUMENT_ROOT']."/infinity/assets/css/main.css");
 
 var_dump($_POST);
+if (isset($_POST["subscriber_email"]))
+{
+inscrireNewsletter($_POST["subscriber_email"]);
+}
 controllerHomePage();
 ?>
