bb77682e62cf65954d118bf91022ea129ac464a8
reapers1996 <damien.forest.via@gmail.com>
2022-06-07 15:01:45 +0200

    Update dataaccess.php

--- dataaccess/dataaccess.php
+++ dataaccess/dataaccess.php
@@ -3,7 +3,7 @@ function AjouterEmailDatabase($pAdresseEmail)
 {
 try
 {
-    $sql = "INSERT INTO newsletter (`email`) value ('$pAdresseEmail');";
+    $sql = "INSERT INTO newsletter (`email`) value (NULL,'$pAdresseEmail');";
     $db= new PDO('mysql:host=localhost;dbname=infinity','root','');
   //  echo $sql;
     //$stmt = $db->prepare($sql);
