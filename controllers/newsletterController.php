59a7287b741390455a1bf74c45255f17a6fd6c07
SarahBddn <sarah.bedreddine.via@gmail.com>
2022-06-07 13:35:36 +0200

    sdrtyuiop
    qsdfghjklmù

--- controllers/newsletterController.php
+++ controllers/newsletterController.php
@@ -1,7 +1,9 @@
 <?php
+require_once($_SERVER["DOCUMENT_ROOT"]."/infinity/dataaccess/dataaccess.php");
+
 function inscrireNewsletter()
 {
     echo "inscrire";
-    
+    ajouterEmailDatabase($pEmail);
 }
 ?>
