70f7089e9eb8daa1db858cca0d76b81ec292d497
reapers1996 <damien.forest.via@gmail.com>
2022-06-07 10:12:27 +0200

    correction require
    correction require

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-require_once("/xampp/htdocs/infinity/controllers/controller.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/infinity/controllers/controller.php");
 controllerHomePage();
 ?>
--- template/homepage.php
+++ template/homepage.php
@@ -9,6 +9,10 @@
 </head>
 
 <body>
+    <div id="titre">
+        <img src="https://drive.google.com/file/d/1LBBWO8utXK8JWR9O0YfW3vysxihbWHLN/view?usp=sharing">
+        <h1>Infinity</h1>
+    </div>
     Bienvenue sur Infinity ! Actualités gaming, classements de jeux
     hebdomadaires, offres à ne pas manquer...quelle que soit votre
     plateforme préférée, nous sommes là pour vous guider !
